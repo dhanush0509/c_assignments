83a72042957de685ae1ed5898b8f469d102d3386
Dhanush Kurapati <dhanushbits@gmail.com>
2020-12-22 15:14:24 +0530

    added prime_number

--- prime_number.c
+++ prime_number.c
@@ -4,13 +4,13 @@ int main()
   int i,a,b;
   for(i = 1; i <= 100; ++i)
   {
-   b = 0;
+   b = 0; // when remainder of a number becomes 0 then b value increases//
     for(a = 1; a <= i; ++a)
     {
-      if ( i%a == 0)
-        b++;
+      if ( i%a == 0) //if remainder becomes 0 which means that number is divisible by that number//
+        b++;// when remainder becomes o b value increases//
     }
-    if(b == 2)
+    if(b == 2)//when a number is divisible by 1 and itself is known as a prime number and b value should be 2//
       printf("%d \n",i);
   }
 }  
