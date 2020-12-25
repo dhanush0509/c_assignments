ce7e9fbc615ccae36edf4222afc6e94f43239e97
Dhanush Kurapati <dhanushbits@gmail.com>
2020-12-25 09:29:41 +0530

    added factorial

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int factorial(int);
+int main()
+{
+   int n;
+   int f = 1;
+   printf("Enter a number \n");
+   scanf("%d",&n);
+   printf("%d = %d\n", n, factorial(n));
+   return 0;
+}
+int factorial(int n)
+{
+   int i;
+   int f = 1;
+   for( i = 1 ; i <= n ; i++ )
+   f = f*i;
+   return f;
+}
